748b6eab9897714ab6d38e759e23605532cf3503
Phan Nguyễn Hữu Phong <112098006+PhongPhan2k4@users.noreply.github.com>
2023-11-23 10:16:33 +0700

    Update app.py

--- offical/app.py
+++ offical/app.py
@@ -16,8 +16,8 @@ st.title('W1 Doppelganger')
 def load():
     text_embedder = TextEmbedding()
     btc = pd.read_csv(os.path.join('data', 'indexing.csv'))
-    extract = pd.read_csv(os.path.join('data', 'extract_indexing.csv')
-    index = faiss.read_index(os.path.join('data', 'faiss_index.faiss')
+    extract = pd.read_csv(os.path.join('data', 'extract_indexing.csv'))
+    index = faiss.read_index(os.path.join('data', 'faiss_index.faiss'))
     translator = Translator()
     return text_embedder, btc, extract, index, translator
 text_embedder, btc, extract, index, translator = load()
